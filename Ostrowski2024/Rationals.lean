154fa0d5f126eac83664e4fc30d20670f76b5e4b
fbarroero <fbarroero@gmail.com>
2024-03-28 17:10:42 +0100

    Progress in arch step 2

--- Ostrowski2024/Rationals.lean
+++ Ostrowski2024/Rationals.lean
@@ -191,18 +191,27 @@ open BigOperators
 variable (m n : ℕ) (hmge : 1 < m) (hnge : 1 < n) (notbdd: ¬ ∀(n : ℕ), f n ≤ 1)
 
 lemma main_inequality : f n ≤ (m * (f m) / ((f m) - 1)) * ((f m) ^ (logb m n)) := by
-  --obtain hn := notbdd_implies_all_gt_one notbdd
-  --have := hn n hnge
+  obtain hm := notbdd_implies_all_gt_one notbdd
+  have : 1< f m := by simp only [hm m hmge]
   set d := Nat.log m n with hd
   have hnmd : f n ≤ m * (∑ i in Finset.range (d + 1), (f m)^i) := by sorry
-  have hsum : ∑ i in Finset.range (d + 1), f ↑m ^ i = (f ↑m ^ (d+1) - 1)/(f ↑m - 1) := by sorry
-  calc f ↑n ≤ m * (∑ i in Finset.range (d + 1), (f m)^i) := by sorry
-    _ = m * (f ↑m ^ (d+1) - 1)/(f ↑m - 1) := by sorry
-    _ ≤ m * (f ↑m ^ (d+1))/(f ↑m - 1) := by sorry
-    _ ≤ ↑m * f ↑m / (f ↑m - 1) * f ↑m ^ d := by sorry
-    _ ≤ ↑m * f ↑m / (f ↑m - 1) * f ↑m ^ logb ↑m ↑n := by sorry
-
-
+  have hsum : ∑ i in Finset.range (d + 1), f ↑m ^ i = (f ↑m ^ (d+1) - 1)/(f ↑m - 1) := by
+    rw [geom_sum_eq]
+    apply ne_of_gt
+    linarith
+  calc f ↑n ≤ m * (∑ i in Finset.range (d + 1), (f m)^i) := hnmd
+    _ = m * (f ↑m ^ (d+1) - 1)/(f ↑m - 1) := by rw [hsum]; ring
+    _ ≤ m * (f ↑m ^ (d+1))/(f ↑m - 1) := by
+      apply div_le_div_of_nonneg_right (by linarith only [hmge, this]) (by linarith only [this])
+    _ = ↑m * f ↑m / (f ↑m - 1) * f ↑m ^ d := by ring
+    _ ≤ ↑m * f ↑m / (f ↑m - 1) * f ↑m ^ logb ↑m ↑n := by
+      apply mul_le_mul_of_nonneg_left
+      rw [←Real.rpow_nat_cast]
+      apply Real.rpow_le_rpow_of_exponent_le (le_of_lt this)
+      have NeededAlsoBefore_MakeALemma : ↑d ≤ logb ↑m ↑n := by sorry
+      exact NeededAlsoBefore_MakeALemma
+      apply div_nonneg _ (by simp only [sub_nonneg]; exact le_of_lt this)
+      exact mul_nonneg (by linarith only [hmge]) (by linarith only [this])
 
 lemma logb_pow (k m n : ℕ) : logb m (n ^ k) = k * logb m n := by
   simp only [logb, log_pow, mul_div]
